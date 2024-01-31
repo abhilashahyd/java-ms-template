4e75bce672a413655ab6cd5ece701d9a7bd97795
Abhilasha <abhilasha.sinha@walkingtree.tech>
2024-01-31 11:27:22 +0530

    first commit

---
+++ src/main/java/tech/walkingtree/javamstemplate/JavaMsTemplateApplication.java
@@ -0,0 +1,13 @@
+package tech.walkingtree.javamstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/walkingtree/javamstemplate/JavaMsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.walkingtree.javamstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
